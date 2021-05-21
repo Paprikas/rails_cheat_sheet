b1d6111fb10b6880a6287e5ced8afaf971d48c81
Maxim Zelenkin <nudepatch@gmail.com>
2021-05-21 08:34:44 +0500

    Make tweets as index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   resources :tweets, except: [:new]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tweets#index"
 end
